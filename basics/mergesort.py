09219e0986a5227e2efca285d39dd422f4a586b0
Leocoh <48472220+Leocoh@users.noreply.github.com>
2020-11-21 22:43:18 +0200

    Update mergesort.py

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -35,8 +35,6 @@ def merge(left, right):
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
 
-            quit(1)
-
     debug_print(value_list=value_list)
 
     sorted_list = mergesort(value_list)
